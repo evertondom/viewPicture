bc110c5cb8f18391cbd2ec5b21a816586106ba0b
evertondom <jony.everton-@hotmail.com>
2022-04-28 09:28:06 -0300

    Buscar Pasta e Selecionar imagem

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Aula2_Solucao_Exe3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscarPasta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnBuscarPasta
+            // 
+            this.btnBuscarPasta.Location = new System.Drawing.Point(313, 39);
+            this.btnBuscarPasta.Name = "btnBuscarPasta";
+            this.btnBuscarPasta.Size = new System.Drawing.Size(114, 20);
+            this.btnBuscarPasta.TabIndex = 1;
+            this.btnBuscarPasta.Text = "Buscar";
+            this.btnBuscarPasta.UseVisualStyleBackColor = true;
+            this.btnBuscarPasta.Click += new System.EventHandler(this.btnBuscarPasta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscarPasta);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBuscarPasta;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula2_Solucao_Exe3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarPasta_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog pastaEscolhida = new FolderBrowserDialog();
+            //alterando a propriedade para exibir o botão Nova pasta
+            pastaEscolhida.ShowNewFolderButton = true;
+
+            DialogResult result = pastaEscolhida.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = pastaEscolhida.SelectedPath;
+                Environment.SpecialFolder root = pastaEscolhida.RootFolder;
+            }
+
+
+        }
+    }
+}
